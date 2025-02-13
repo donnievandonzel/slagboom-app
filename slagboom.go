06d8597b6536748ce11a244dd23c957b9ea89254
donnievandonzel <554057@student.fontys.nl>
2025-02-13 11:09:04 +0100

    t

--- slagboom.go
+++ slagboom.go
@@ -7,7 +7,9 @@ import (
 	"time"
 )
 
-func getGroot() string {
+var allowedKentekens = []string{"AB-123-CD", "EF-456-GH", "XY-789-ZZ"}
+
+func getGroet() string {
 	now := time.Now()
 	hour := now.Hour()
 
@@ -22,18 +24,35 @@ func getGroot() string {
 	}
 }
 
+func kentekenIsAllowed(kenteken string) bool {
+	for _, k := range allowedKentekens {
+		if k == kenteken {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	flag.Parse()
 
-	if flag.NArg() == 0 {
-		groet := getGroot()
-		if groet == "" {
-			fmt.Println("Sorry, de parkeerplaats is 's nachts gesloten.")
+	if flag.NArg() == 1 {
+		kenteken := flag.Arg(0)
+		if kentekenIsAllowed(kenteken) {
+			groet := getGroet()
+			if groet == "" {
+				fmt.Println("Sorry, de parkeerplaats is 's nachts gesloten.")
+			} else {
+				fmt.Printf("%s! Welkom bij Fonteyn Vakantieparken\n", groet)
+			}
 		} else {
-			fmt.Printf("%s! Welkom bij Fonteyn Vakantieparken\n", groet)
+			fmt.Println("U heeft helaas geen toegang tot het parkeerterrein")
 		}
-	} else {
+	} else if flag.NArg() > 1 {
 		fmt.Println("Onbekende parameters")
+		os.Exit(1)
+	} else {
+		fmt.Println("Geen kenteken opgegeven")
 		os.Exit(1)
 	}
 }
